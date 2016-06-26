136c4074d8dcfc232ce064d667540a6d059ef8ff
Duerton <duerton@gmail.com>
2016-06-26 15:14:13 -0300

    Bilheteria

--- Bilheteria/src/bilheteria/Compra.java
+++ Bilheteria/src/bilheteria/Compra.java
@@ -1,5 +1,8 @@
 package bilheteria;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Compra {
 
     private static int totalIDCompra;
@@ -7,17 +10,23 @@ public class Compra {
     private final Espetaculo espetaculo;
     private final Cliente cliente;
     private final int quantidade;
+    private final Date data;
 
-    public Compra(Espetaculo espetaculo, Cliente cliente, int quantidade) {
+    public Compra(Espetaculo espetaculo, Cliente cliente, int quantidade, Date data) {
         this.espetaculo = espetaculo;
         this.cliente = cliente;
         this.quantidade = quantidade;
         Compra.totalIDCompra++;
         this.IDCompra = totalIDCompra;
+        this.data = data;
     }
 
-    public boolean conferirDadosCartao(String nomecartao, int numcartao, int validade, int codigo) {
-        return true;
+    public boolean ligarCartao(String nomecartao, String numcartao, String validade, String codigo) {
+        CartaoDeCredito cartao = new CartaoDeCredito();
+        if(cartao.conferirDadosCartao(nomecartao, numcartao, validade, codigo)){
+            return true;
+        }
+        return false;        
     }
 
     public Espetaculo getEspetaculo() {
@@ -43,11 +52,14 @@ public class Compra {
         System.out.println("Titulo: " + espetaculo.getTipo());
         System.out.println("Cidade: " + espetaculo.getCidade());
         System.out.println("Endereco: " + espetaculo.getEndereco());
-        System.out.println("Valor do ingresso: " + espetaculo.getValorIngresso());
+        System.out.println("Valor do ingresso: R$" + espetaculo.getValorIngresso());
         System.out.println("###################");
         System.out.println("Quantidade de ingressos comprados: " + this.quantidade);
-        System.out.println("Valor de cada ingresso: " + espetaculo.getValorIngresso());
-        System.out.println("Total: " + (this.quantidade * espetaculo.getValorIngresso()));
+        System.out.println("Valor de cada ingresso: R$" + espetaculo.getValorIngresso());
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String dataS = sdf.format(this.data);
+        System.out.println("Data do Espetaculo: " + dataS);
+        System.out.println("Total: R$" + (this.quantidade * espetaculo.getValorIngresso()));
         System.out.println("###################");
     }
 }
--- Bilheteria/src/bilheteria/main.java
+++ Bilheteria/src/bilheteria/main.java
@@ -1,64 +1,35 @@
 package bilheteria;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class main {
 
     public static void main(String[] args) {
-        boolean teste = true;
+
         BD bd = new BD();
         Tela telas = new Tela();
-        Scanner teclado = new Scanner(System.in);
-        
-        
+
         Espetaculo espetaculo1 = new Espetaculo("Matriz", "Cinema", "Niterói", "Na esquina", "Futurista", 20.00f, 200, "10/11/2015", "15/06/2016");
         Espetaculo espetaculo2 = new Espetaculo("O relógio", "Teatro", "Rio de Janeiro", "Outra esquina", "Drama", 25.00f, 150, "14/03/2016", "20/06/2016");
-        Espetaculo espetaculo3 = new Espetaculo("Rei Leão", "Teatro Infantil", "São Gonçalo", "Outra esquina", "Animação", 15.00f, 200, "12/02/2016", "15/06/2017");
+        //Espetaculo espetaculo3 = new Espetaculo("Rei Leão", "Teatro Infantil", "São Gonçalo", "Outra esquina", "Animação", 15.00f, 200, "12/02/2016", "15/06/2017");
         bd.inserirEspetaculos(espetaculo1);
         bd.inserirEspetaculos(espetaculo2);
-        bd.inserirEspetaculos(espetaculo3);
-        
-        bd.retirarFilmesAntigos();
-        
+        //bd.inserirEspetaculos(espetaculo3);
+
         Cliente auxiliar = new Cliente("duerton", 30, "duerton", "root");
         bd.inserirCliente("duerton", auxiliar);
-        
-        
+
+        boolean teste = true;
         while (teste) {
-            System.out.println(" ");
-            System.out.println("Bem vindo a Bilheteria");
-            System.out.println("Veja abaixo os espetáculos disponiveis");
-            System.out.println("Para aquisição de ingresso é necessário estar logado");
-            System.out.println(" ");
-            System.out.println("##########################");
-            bd.visualizarEspetaculos();
-            System.out.println("1- Novo cadastro");
-            System.out.println("2- Logar");
-            System.out.println("3- Sair");
-            int entrada = teclado.nextInt();
-            switch (entrada) {
-                case 1:
-                    telas.cadastrarCliente(bd);
-                    break;
-                case 2:
-                    System.out.println("Informe o login");
-                    String login = teclado.next();
-                    if (bd.confereLogin(login)) {
-                        Cliente cliente = bd.buscaCliente(login);
-                        if (telas.loginCliente(cliente)) {
-                            telas.visualizacaoCliente(cliente,bd);
-                        }
-                    } else {
-                        System.out.println("Login inexistente");
-                    }   break;
-                case 3:
-                    System.out.println("Até a próxima");
-                    teste = false;
-                    break;
-                default:
-                    System.out.println("Opcao inválida");
-                    break;
+            try {
+                telas.telaInicial(bd);
+                teste = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Opcao inválida");
             }
         }
+
     }
 }
